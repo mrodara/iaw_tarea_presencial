abefe986d996b1f7c98cf4343f32114bafd56b72
Manu Rodríguez <manueljesus.rodriguez@iesalandalus.org>
2022-04-27 19:20:03 +0200

    Creación estructura + bbdd

--- index.php
+++ index.php
@@ -8,7 +8,34 @@
     <title>Tarea Presencial IAW</title>
 </head>
 <body>
-    
+    <!-- HEADER-->
+    <?php include("./layouts/header.php"); ?>
+    <!-- END HEADER-->
+
+    <!-- ENUNCIADO TAREA -->
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Enunciado de la Tarea</h1>
+        <p class="col-md-10 fs-4">
+            Realiza un CRUD completo con la tabla Usuarios que has importado tal y como se indicó en las instrucciones enviadas 
+            para ello dispones de los ficheros <b>show.php (lo usarás para listar todos los usuarios de la BBDD), create.php, edit.php y delete.php</b> en los 
+            que deberás incluir el código correspondiente en los lugares indicados.
+        </p>
+        <p class="col-md-10 fs-4">
+            Es importante que el código sea lo más legible y eficiente posible. Tenlo presente.
+        </p>
+      </div>
+    </div>
+    <!-- FIN ENUNCIADO TAREA -->
+
+
+    <!-- FOOTER -->
+    <div class="footer p-5 mb-4 bg-primary">
+        <p class="fs-5">Tarea Presencial IAW 3er Trimestre</p>
+    </div>
+    <!-- END FOOTER -->
+
+
     <script src="http://tpiaw.com/js/bootstrap.min.js"></script>
 </body>
 </html>
